e0660236bdeff9860c20dfdbe3dd497b4000f164
Diponkor99 <roydiponker891@gmail.com>
2024-10-08 10:55:38 +0600

    daynamik card

--- in.js
+++ in.js
@@ -1,22 +1,12 @@
+// let numbers = [1000, 100, 500, 300, 400, 100];
 
-// category: "Cat"
-// category_icon: "https://i.ibb.co.com/N7dM2K1/cat.png"
-// id : 1
-// [[Prototype]]
-// : 
-// Object
+// numbers.sort(function(a, b) {
+//     return b - a;  // This sorts the array in descending order
+// });
+
+// console.log(numbers);
 
 
-
-// {petId: 1, breed: 'Golden Retriever', category: 'Dog', date_of_birth: '2023-01-15', price: 1200, …}
-
-// breed: "Golden Retriever"
-// category: "Dog"
-// date_of_birth: "2023-01-15"
-// gender: "Male"
-// image: "https://i.ibb.co.com/p0w744T/pet-1.jpg"
-// petId: 1
-// pet_details: "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog."
-// pet_name: "Sunny"
-// price: 1200
-// vaccinated_status: "Fully"
+// let myArray = [1, 2, 3];
+// myArray.push(4);  // myArray এখন হবে [1, 2, 3, 4]
+// console.log(myArray);
--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 
+
 // all button  js fetch lodefile start
 const lodeContainer=()=>{
     fetch('https://openapi.programming-hero.com/api/peddy/categories')
@@ -49,12 +50,27 @@ loadepasts();
 const displaypasts=(id)=>{
   const card=document.getElementById('card');
   card.innerHTML="";
-  id.forEach((aitem) => {
-   console.log(aitem)
- 
-  //  console.log(aitem.image)
-   const addcard=document.createElement('div');
+  //  console.log(id)
+if(id.length==0){
   
+  card.classList.remove('grid')
+  card.innerHTML=`
+  <div class="mt-20 mb-32 mx-auto text-center w-11/12">
+  <img class="w-5/12 mx-auto" src="images/error.webp" alt="">
+  <h1 class="font-bold text-3xl">No Information Available</h1>
+  <p class="text-base font-normal">It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
+its layout. The point of using Lorem Ipsum is that it has a.</p>
+  </div>
+ `
+
+  return;
+}
+
+
+  id.forEach((aitem) => {
+  //  console.log(aitem.petId)
+   const addcard=document.createElement('div');
+  //  console.log(aitem.price)
    addcard.innerHTML=`<div class="card m-5 bg-base-100  shadow-xl  ">
   <figure class="px-10 pt-10 " >
     <img
@@ -64,13 +80,16 @@ const displaypasts=(id)=>{
   </figure>
   <div class="card-body  ">
     <h2 class="font-bold text-[#131313] text-xl">${aitem.pet_name}</h2>
-    <h1 class="text-base font-normal text-slate-600"><span><i class="fa-solid fa-qrcode"></i><span>  breed : ${aitem.breed}</h>
+   <h1 class="text-base font-normal text-slate-600"><span><i class="fa-solid fa-qrcode"></i><span>  breed : ${aitem.breed==null ? "Not available":aitem.breed}</h>
+   
+     <h1 class="text-base font-normal text-slate-600"><i class="fa-regular fa-calendar"></i><span>  Birth : ${aitem.date_of_birth==null ? "Not available":aitem.date_of_birth}</h>
 
-    <h1 class="text-base font-normal text-slate-600"><i class="fa-regular fa-calendar"></i><span>  Birth : ${aitem.date_of_birth}</h>
 
-      <h1 class="text-base font-normal text-slate-600"><i class="fa-solid fa-transgender"></i><span>  Gender : ${aitem.gender}</h>
+   
+
+      <h1 class="text-base font-normal text-slate-600"><i class="fa-solid fa-transgender"></i><span>  Gender : ${aitem.gender==null ? "Not available":aitem.gender}</h>
     
-      <h1 class="text-base font-normal text-slate-600"><i class="fa-solid fa-dollar-sign"></i>  Price : ${aitem.price}$ </h>
+      <h1 class="text-base font-normal text-slate-600"><i class="fa-solid fa-dollar-sign"></i>  Price : ${aitem.price==null ? "Not available":aitem.price}$ </h>
 
        <div class="divider"></div>
 <div class="card-actions">
@@ -78,7 +97,7 @@ const displaypasts=(id)=>{
       <img class="w-10 " src="images/like.webp" alt="">
       </button>
       <button class="btn text-lime-900 font-bold text-lg">Adopt</button>
-      <button class="btn text-lime-900 font-bold text-lg">Details</button>
+      <button onclick="CardDetails(${aitem.petId})" class="btn text-lime-900 font-bold text-lg">Details</button>
     </div>
   </div>
 </div>
@@ -90,13 +109,15 @@ const displaypasts=(id)=>{
   })
 }
 
-
+// displaypasts(data.data)
 // button chcae your containar start
 const Capbutton=((id)=>{
  document.getElementById('loading').style.display='none'
  fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
  .then((res)=>res.json())
- .then((data)=>displaypasts(data.data))
+ .then((data)=>{displaypasts(data.data)
+ })
+
  .catch(error=>console.log(error))
 })
 // button chcae your containar End
@@ -109,8 +130,7 @@ const likedisplay =(id)=>{
        
        
       addImages.innerHTML=`
-      <img class="p-2 h-full w-22 rounded-xl" src="${id}"/>
-      `
+      <img class="p-2 h-full w-22 rounded-xl card bg-base-100  shadow-xl" src="${id}"/>      `
       sectionContent.appendChild(addImages)
 }
 
@@ -120,8 +140,51 @@ const loadContainar=(id)=>{
   setTimeout(function(){
     document.getElementById('containerpic').style.display='block';
     Capbutton(id);
-  },1000)
+  },2000)
 
 }
+// Details button click  id your id 
+const CardDetails=async(id)=>{
+  const uri=`https://openapi.programming-hero.com/api/peddy/pet/${id}`
+  const res=await fetch(uri);
+  const data = await res.json();
+  dispalayCard(data.petData)
+}
+
+const dispalayCard=(id)=>{
+  const content=document.getElementById('modeldatelis');
+  content.innerHTML=`
+  <img class="mb-4 min-w-full"  src=${id.image}/>
+<h2 class=" mb-4 text-2xl font-semibold">${id.pet_name==null ? "Not available":id.pet_name} </h2>
+
+<div class="flex mb-4">
+<div>
+<p><i class="fa-solid  fa-qrcode "></i>    Breed: ${id.breed==null ? "Not available":id.breed}</p>
+  <p ><i class="fa-regular fa-calendar"></i>  birth: ${id.date_of_birth==null ? "Not available":id.date_of_birth}</p>
+  <p ><i class="fa-solid fa-transgender"></i> Gender: ${id.gender==null ? "Not available":id.gender}</p>
+
+</div>
+<div>
+<p class="ml-10 "><i class="fa-solid fa-dollar-sign"></i> price: ${id.price==null ? "Not available":id.price} <i class="fa-solid fa-dollar-sign"></i></p>
+<p class="ml-10  "><i class="fa-solid fa-transgender"></i> Vaccinated Status: ${id.vaccinated_status==null ? "Not available":id.vaccinated_status}</p>
+
+</div>
+</div>
+<div class="divider"></div>
+<h1 class="font-semibold mb-4 text-4xl">Details Information</h1>
+<h2>${id.pet_details==null ? "Not available":id.pet_details} </h2>
+
+  `
+  document.getElementById("myModal").showModal();
+
+} 
+// sort by price funtion
+  const SortButton=()=>{
+    alert('add sort price')
+  }
+
+
+
+
 
 
